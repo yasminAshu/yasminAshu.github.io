b3df93ad9fd99338c465900d761353e7e4c70b47
yasmin <yasminagwan98@gmail.com>
2019-12-01 01:17:29 +0530

    Updates

---
+++ precache-manifest.ae9f986375394bebd4ee9af27a467175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28b0ce3fe7d4d9bddd8d9c2aeff07ee",
+    "url": "/yasminAshu.github.io/index.html"
+  },
+  {
+    "revision": "dd5a05fe4dec942e6c1f",
+    "url": "/yasminAshu.github.io/static/css/main.0ac2ae4d.chunk.css"
+  },
+  {
+    "revision": "7e8dec7b21492a9407da",
+    "url": "/yasminAshu.github.io/static/js/2.561fa94d.chunk.js"
+  },
+  {
+    "revision": "dd5a05fe4dec942e6c1f",
+    "url": "/yasminAshu.github.io/static/js/main.9f4f6568.chunk.js"
+  },
+  {
+    "revision": "73d87085294b3509266f",
+    "url": "/yasminAshu.github.io/static/js/runtime-main.99912ff3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/starWars/precache-manifest.fe3dd594c5fa81eaa8a428c0c22d33a9.js"
+  "/yasminAshu.github.io/precache-manifest.ae9f986375394bebd4ee9af27a467175.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starWars/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yasminAshu.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
