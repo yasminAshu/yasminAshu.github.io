3d98884d33163050f0d21337d460235349fb8d25
yasmin <yasminagwan98@gmail.com>
2019-11-30 23:55:58 +0530

    Updates

---
+++ precache-manifest.f1e3215db4dc4e69aff024fb02290dce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff41f80a2906f1b732248eb0c0531c0",
+    "url": "/starWars/index.html"
+  },
+  {
+    "revision": "2fdb3120f4974debdab8",
+    "url": "/starWars/static/css/main.0ac2ae4d.chunk.css"
+  },
+  {
+    "revision": "6194bdeee2282d07344a",
+    "url": "/starWars/static/js/2.9877051c.chunk.js"
+  },
+  {
+    "revision": "2fdb3120f4974debdab8",
+    "url": "/starWars/static/js/main.69ba5481.chunk.js"
+  },
+  {
+    "revision": "8858a3087b2c6024fdeb",
+    "url": "/starWars/static/js/runtime-main.4cf021c3.js"
+  }
+]);
